ad38118b0927a6899848a035030aced8337fa530
tewereus <tewereus12312@gmail.com>
2025-01-28 11:09:26 +0300

    started post service

--- media-service/src/utils/logger.js
+++ media-service/src/utils/logger.js
@@ -8,7 +8,7 @@ const logger = winston.createLogger({
     winston.format.splat(),
     winston.format.json()
   ),
-  defaultMeta: { service: "server" },
+  defaultMeta: { service: "media-service" },
   transports: [
     new winston.transports.Console({
       format: winston.format.combine(
---
+++ post-service/src/models/postModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+// Declare the Schema of the Mongo model
+var postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    mediaUrls: [
+      {
+        type: String,
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+postSchema.index({ content: "text" });
+//Export the model
+module.exports = mongoose.model("Post", postSchema);
