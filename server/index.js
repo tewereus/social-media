b6e1bbd2b8fff7559d6b16dd4567757643b163a7
tewereus <tewereus12312@gmail.com>
2025-02-03 11:14:13 +0300

    added auth middleware

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { RedisStore } = require("rate-limit-redis");
 const logger = require("./utils/logger");
 const proxy = require("express-http-proxy");
 const errorHandler = require("./middlewares/errorhandler");
-// const { validateToken } = require("./middlewares/authMiddleware");
+const { validateToken } = require("./middlewares/authMiddleware");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -75,26 +75,26 @@ app.use(
 );
 
 //setting up proxy for our post service
-// app.use(
-//   "/v1/posts",
-//   validateToken,
-//   proxy(process.env.POST_SERVICE_URL, {
-//     ...proxyOptions,
-//     proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
-//       proxyReqOpts.headers["Content-Type"] = "application/json";
-//       proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+app.use(
+  "/v1/posts",
+  validateToken,
+  proxy(process.env.POST_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
 
-//       return proxyReqOpts;
-//     },
-//     userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
-//       logger.info(
-//         `Response received from Post service: ${proxyRes.statusCode}`
-//       );
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response received from Post service: ${proxyRes.statusCode}`
+      );
 
-//       return proxyResData;
-//     },
-//   })
-// );
+      return proxyResData;
+    },
+  })
+);
 
 //setting up proxy for our media service
 // app.use(
